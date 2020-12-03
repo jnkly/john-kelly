f4e9d9707ca44ca79155f2d0c87ffe9dfc9ac902
John Kelly <johnkellydesign@gmail.com>
2020-12-03 17:55:35 +0000

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    purge: {
+        mode: 'layers',
+        content: ['./public/**/*.html'],
+    },
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
